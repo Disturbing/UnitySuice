eb1a6e96839ca41da6289e18ec5e2e78c00524c6
Joseph Cooper <joseph@jungleegames.com>
2014-12-26 22:11:55 +0800

    Added Unity Event Examples.

---
+++ SuiceExample/Assets/UnitySuiceCommons/Scripts/UnityEvent/IUnityEventExample.cs
@@ -0,0 +1,15 @@
+﻿using DTools.Suice;
+
+namespace UnitySuiceCommons.UnityEvent
+{
+    /// <summary>
+    /// We use interfaces all the time to not only allow testability, but to hide event calls so that other's may not accidently call it.
+    /// 
+    /// @author DisTurBinG
+    /// </summary>
+    [ImplementedBy(typeof(UnityEventExample))]
+    public interface IUnityEventExample
+    {
+
+    }
+}
---
+++ SuiceExample/Assets/UnitySuiceCommons/Scripts/UnityEvent/UnityEventExample.cs
@@ -0,0 +1,22 @@
+﻿using DTools.Suice;
+using UnityEngine;
+using UnitySuiceCommons.EventDispatcher;
+using UnitySuiceCommons.EventDispatcher.Unity.UnityEvent;
+
+namespace UnitySuiceCommons.UnityEvent
+{
+    /// <summary>
+    /// Example of listening to unity events automatically using Suice!
+    /// 
+    /// @author DisTurBinG
+    /// </summary>
+    [Singleton]
+    public class UnityEventExample : IUnityEventExample
+    {
+        [EventListener]
+        public void OnStart(UnityStartEvent startEvent)
+        {
+            Debug.Log("Unity Start Event Called!");
+        }
+    }
+}
